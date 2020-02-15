654512f13330dfbf5fa538edef465e80ea03b512
snyjen <you@example.com>
2020-02-14 21:40:43 -0800

    changes

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,4 +1,18 @@
-function windChill(tempF, speed) {
-	var f=35.74+(0.6215*tempF)- 35.75*(Math.pow(speed,0.16))+(0.4275*tempF*Math.pow(speed,0.16)) 
-	return f;
-		}	
+var tempF=parseInt(document.getElementById("tempF").innerText);
+var speed=parseInt(document.getElementById("speed").innerText);
+
+
+if(tempF <=50 && speed >= 3) {
+    var f=35.74+(0.6215*tempF)- 35.75*(Math.pow(speed,0.16))+(0.4275*tempF*Math.pow(speed,0.16))
+    
+    document.getElementById("tempF").innerHTML=f.toFixed(2) + "&#8457;";
+
+}
+
+else {
+    document.getElementById("tempF").innerText="N/A"
+}
+
+
+	
+	
