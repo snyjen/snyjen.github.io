6547a94bc5d4925a9b8ebdb76daf48ed4f5e0750
snyjen <you@example.com>
2020-02-14 21:45:08 -0800

    changes

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -5,12 +5,12 @@ var speed=parseInt(document.getElementById("speed").innerText);
 if(tempF <=50 && speed >= 3) {
     var f=35.74+(0.6215*tempF)- 35.75*(Math.pow(speed,0.16))+(0.4275*tempF*Math.pow(speed,0.16))
     
-    document.getElementById("tempF").innerHTML=f.toFixed(2) + "&#8457;";
+    document.getElementById("windF").innerHTML=f.toFixed(2);
 
 }
 
 else {
-    document.getElementById("tempF").innerText="N/A"
+    document.getElementById("windF").innerText="N/A"
 }
 
 
