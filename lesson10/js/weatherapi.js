b298e0eee3830434b192993145307c71eb79045b
snyjen <you@example.com>
2020-03-12 12:13:52 -0700

    changes

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,12 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=36abeb743432af50afc71f9895b9a4af';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+  	const desc = jsObject.weather[0].description;
+  	document.getElementById('imagesrc').textContent = imagesrc;
+  	document.getElementById('icon').setAttribute('src', imagesrc);
+  	document.getElementById('icon').setAttribute('alt', desc);
+  });
