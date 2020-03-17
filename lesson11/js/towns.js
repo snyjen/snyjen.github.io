cbccf11141da631f46bf2d3b45afe5a61a92e3c4
snyjen <you@example.com>
2020-03-17 13:45:08 -0700

    changes

---
+++ lesson11/js/towns.js
@@ -0,0 +1,49 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+
+    const towns = jsonObject['towns'];
+
+    for (let i = 0; i < towns.length; i++ ) {
+        
+      if (towns[i].name=="Fish Haven" || towns[i].name=="Preston" || towns[i].name=="Soda Springs") {
+
+        let card = document.createElement('section');
+        let name = document.createElement('h3');
+        let motto = document.createElement('h4');
+        let yearFounded = document.createElement('p');
+        let currentPopulation = document.createElement('p');
+        let averageRainfall = document.createElement('p');
+        let photo = document.createElement('img');
+
+        name.textContent = towns[i].name;
+
+        motto.textContent = towns[i].motto;       
+
+        yearFounded.textContent = "Year Founded:" + towns[i].yearFounded;
+      
+        currentPopulation.textContent = "Population:" + towns[i].currentPopulation;
+
+        averageRainfall.textContent = "Annual Rainfall:" + towns[i].averageRainfall;
+      
+        let alt = towns[i].photo;
+        photo.setAttribute('src', "images/" + towns[i].photo);
+        photo.setAttribute('alt', alt);
+        
+        card.appendChild(name);
+        card.appendChild(motto);
+        card.appendChild(yearFounded);
+        card.appendChild(currentPopulation);
+        card.appendChild(averageRainfall);
+        card.appendChild(photo);
+       
+        document.querySelector('div.cards').appendChild(card);
+
+      } 
+    }
+});
