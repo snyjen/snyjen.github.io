330d272302c01698ec35b6e6209a8224bb8fed20
snyjen <you@example.com>
2020-04-06 14:26:54 -0700

    changes

---
+++ final/js/riggins.js
@@ -0,0 +1,42 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=83549,us&units=imperial&APPID=3e0425f4b4feb5fd17b29e3a168f2265';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+      document.getElementById('currently').textContent = jsObject.weather[0].main;
+      document.getElementById('high').textContent = jsObject.main.temp;
+      document.getElementById('humidity').textContent = jsObject.main.humidity;
+      document.getElementById('wind').textContent = jsObject.wind.speed;
+
+      let tempF = parseFloat(document.getElementById ("high").innerHTML);
+      let wind = parseFloat(document.getElementById ("wind").innerHTML);
+
+      if(tempF <=50 && speed >= 3) {
+        var f=35.74+(0.6215*tempF)- 35.75*(Math.pow(speed,0.16))+(0.4275*tempF*Math.pow(wind,0.16))
+        
+        document.getElementById("windchill").innerHTML=f.toFixed(2);
+    
+    }
+    
+    else {
+        document.getElementById("windchill").innerText="N/A"
+    }
+    
+  }); 
+
+
+  var tempF=parseInt(document.getElementById("tempF").innerText);
+var speed=parseInt(document.getElementById("speed").innerText);
+
+
+if(tempF <=50 && speed >= 3) {
+    var f=35.74+(0.6215*tempF)- 35.75*(Math.pow(speed,0.16))+(0.4275*tempF*Math.pow(speed,0.16))
+    
+    document.getElementById("windF").innerHTML=f.toFixed(2);
+
+}
+
+else {
+    document.getElementById("windF").innerText="N/A"
+}
